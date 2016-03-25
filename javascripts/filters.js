84c6837860f66b86b2f1a95d9aba0f19f893774b
Pedro Rogério <pinceladasdaweb@hotmail.com>
2016-03-25 20:51:11 -0300

    Update example

---
+++ javascripts/filters.js
@@ -0,0 +1,49 @@
+VanillaGram('.filter1', {
+    filter: 'fade'
+});
+
+VanillaGram('.filter2', {
+    filter: 'vintage'
+});
+
+VanillaGram('.filter3', {
+    filter: 'greyscale'
+});
+
+VanillaGram('.filter4', {
+    filter: 'luxen',
+    lightleak: 'lightleak2'
+});
+
+VanillaGram('.filter5', {
+    filter: 'subtle'
+});
+
+VanillaGram('.filter6', {
+    filter: 'olive'
+});
+
+VanillaGram('.filter7', {
+    filter: 'beach'
+});
+
+VanillaGram('.filter8', {
+    filter: 'oldtimey',
+    shadow: 'drama'
+});
+
+VanillaGram('.filter9', {
+    lightleak: 'lightleak1'
+});
+
+VanillaGram('.filter10', {
+    lightleak: 'lightleak2'
+});
+
+VanillaGram('.filter11', {
+    shadow: 'vignette'
+});
+
+VanillaGram('.filter12', {
+    lightleak: 'lightleak3'
+});
