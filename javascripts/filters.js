37e1459c3894a1ebf89f9978567e9c2eb6c1da5c
Pedro Rogério <pinceladasdaweb@hotmail.com>
2016-03-26 14:29:59 -0300

    Update example

--- javascripts/filters.js
+++ javascripts/filters.js
@@ -1,49 +1,51 @@
-VanillaGram('.filter1', {
-    filter: 'fade'
-});
+window.addEventListener('load', function () {
+    VanillaGram('.filter1', {
+        filter: 'fade'
+    });
 
-VanillaGram('.filter2', {
-    filter: 'vintage'
-});
+    VanillaGram('.filter2', {
+        filter: 'vintage'
+    });
 
-VanillaGram('.filter3', {
-    filter: 'greyscale'
-});
+    VanillaGram('.filter3', {
+        filter: 'greyscale'
+    });
 
-VanillaGram('.filter4', {
-    filter: 'luxen',
-    lightleak: 'lightleak2'
-});
+    VanillaGram('.filter4', {
+        filter: 'luxen',
+        lightleak: 'lightleak2'
+    });
 
-VanillaGram('.filter5', {
-    filter: 'subtle'
-});
+    VanillaGram('.filter5', {
+        filter: 'subtle'
+    });
 
-VanillaGram('.filter6', {
-    filter: 'olive'
-});
+    VanillaGram('.filter6', {
+        filter: 'olive'
+    });
 
-VanillaGram('.filter7', {
-    filter: 'beach'
-});
+    VanillaGram('.filter7', {
+        filter: 'beach'
+    });
 
-VanillaGram('.filter8', {
-    filter: 'oldtimey',
-    shadow: 'drama'
-});
+    VanillaGram('.filter8', {
+        filter: 'oldtimey',
+        shadow: 'drama'
+    });
 
-VanillaGram('.filter9', {
-    lightleak: 'lightleak1'
-});
+    VanillaGram('.filter9', {
+        lightleak: 'lightleak1'
+    });
 
-VanillaGram('.filter10', {
-    lightleak: 'lightleak2'
-});
+    VanillaGram('.filter10', {
+        lightleak: 'lightleak2'
+    });
 
-VanillaGram('.filter11', {
-    shadow: 'vignette'
-});
+    VanillaGram('.filter11', {
+        shadow: 'vignette'
+    });
 
-VanillaGram('.filter12', {
-    lightleak: 'lightleak3'
-});
+    VanillaGram('.filter12', {
+        lightleak: 'lightleak3'
+    });
+})
